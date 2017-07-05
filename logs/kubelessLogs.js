3f2db75b5902a5199b723ff7c74b0038ce3592ac
Andres <andres@bitnami.com>
2017-07-05 16:58:11 +0200

    Add support to 'log' command

---
+++ logs/kubelessLogs.js
@@ -0,0 +1,146 @@
+'use strict';
+
+const _ = require('lodash');
+const Api = require('kubernetes-client');
+const BbPromise = require('bluebird');
+const helpers = require('../lib/helpers');
+const moment = require('moment');
+
+class KubelessLogs {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options || {};
+    this.provider = this.serverless.getProvider('google');
+    this.commands = {
+      logs: {
+        usage: 'Output the logs of a deployed function',
+        lifecycleEvents: [
+          'logs',
+        ],
+        options: {
+          count: {
+            usage: 'Number of lines to print',
+            shortcut: 'n',
+          },
+        },
+      },
+    };
+    this.hooks = {
+      'logs:logs': () => BbPromise.bind(this)
+        .then(this.validate)
+        .then(this.logsFunction)
+        .then(() => {
+          if (this.options.tail) {
+            let m = moment.utc().valueOf();
+            setInterval(() => {
+              this.printLogs({
+                startTime: m,
+                count: null,
+              });
+              m = moment.utc().valueOf();
+            }, this.options.interval || 1000);
+          }
+        }),
+    };
+  }
+
+  validate() {
+    helpers.validateEnv();
+    const unsupportedOptions = ['stage', 'region'];
+    helpers.warnUnsupportedOptions(
+      unsupportedOptions,
+      this.options,
+      this.serverless.cli.log.bind(this.serverless.cli)
+    );
+    return BbPromise.resolve();
+  }
+
+  filterLogs(logs, options) {
+    const opts = _.defaults({}, options, {
+      startTime: null,
+      count: null,
+      filter: null,
+    });
+    let logEntries = _.compact(logs.split('\n'));
+    if (opts.count) {
+      logEntries = logEntries.slice(logEntries.length - opts.count);
+    }
+    if (opts.filter) {
+      logEntries = _.filter(logEntries, entry => !!entry.match(opts.filter));
+    }
+    if (opts.startTime) {
+      const since = !!opts.startTime.toString().match(/(?:m|h|d)/);
+      let startMoment = null;
+      if (since) {
+        startMoment = moment.utc().subtract(
+          opts.startTime.replace(/\D/g, ''),
+          opts.startTime.replace(/\d/g, '')
+        ).valueOf();
+      } else {
+        startMoment = moment.utc(opts.startTime).valueOf();
+      }
+      const logIndex = _.findIndex(logEntries, entry => {
+        const entryDate = entry.match(/(\d{2}\/[a-zA-Z]{3}\/\d{4}:\d{2}:\d{2}:\d{2})/);
+        if (entryDate) {
+          const entryMoment = moment.utc(entryDate[1], 'DD/MMM/YYYY:hh:mm:ss').valueOf();
+          return entryMoment >= startMoment;
+        }
+        return false;
+      });
+      if (logIndex > -1) {
+        logEntries = logEntries.slice(logIndex);
+      } else {
+        // There is no entry after the given startTime
+        logEntries = [];
+      }
+    }
+    return logEntries.join('\n');
+  }
+
+  printLogs(options) {
+    const opts = _.defaults({}, options, {
+      startTime: this.options.startTime,
+      count: this.options.count,
+      filter: this.options.filter,
+      silent: false,
+    });
+    const core = new Api.Core(
+      Object.assign(helpers.getMinikubeCredentials(), {
+        url: process.env.KUBE_API_URL,
+        group: 'k8s.io',
+      })
+    );
+    return new BbPromise((resolve, reject) => {
+      core.ns.pods.get((err, podsInfo) => {
+        if (err) reject(err);
+        const functionPod = _.find(
+          podsInfo.items,
+          (podInfo) => podInfo.metadata.labels.function === this.options.function
+        );
+        if (!functionPod) {
+          reject(
+            `Unable to find the pod for the function ${this.options.function}. ` +
+            'Please ensure that there is a function deployed with that ID'
+          );
+        }
+        core.ns.pods(functionPod.metadata.name).log.get((errLog, logs) => {
+          if (errLog) reject(errLog);
+          const filteredLogs = this.filterLogs(logs, opts);
+          if (!_.isEmpty(filteredLogs)) {
+            if (!opts.silent) {
+              console.log(filteredLogs);
+            }
+          }
+          return BbPromise.resolve(filteredLogs);
+        });
+      });
+    });
+  }
+
+  logsFunction() {
+    this.serverless.cli.log(`Retrieving logs of ${this.options.function}...`);
+    this.printLogs();
+  }
+}
+
+module.exports = KubelessLogs;
