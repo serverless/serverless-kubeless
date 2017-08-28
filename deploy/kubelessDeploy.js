35f05a47f0a52b4c4b44fe67164abf70a11840e6
Andres <andres@bitnami.com>
2017-08-28 12:40:50 +0200

    Do not make the user info mandatory

--- deploy/kubelessDeploy.js
+++ deploy/kubelessDeploy.js
@@ -401,6 +401,12 @@ class KubelessDeploy {
                           // don't try to add an ingress rule
                           return new BbPromise((r) => r());
                         }
+                        if (_.isEmpty(event[eventType])) {
+                          throw new Error(
+                            `Wrong defintion for the event ${event}. ` +
+                            'Expecting an Object with valid keys.'
+                          );
+                        }
                         return this.addIngressRuleIfNecessary(
                           name,
                           eventType,
