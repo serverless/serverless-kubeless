b1b2abf2125c4214349bca28dfc0298d78219ee7
Andres <andres@bitnami.com>
2017-08-31 10:41:20 +0200

    Move npm installation

--- scripts/install-test-environment.sh
+++ scripts/install-test-environment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 install_kubectl() {
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
