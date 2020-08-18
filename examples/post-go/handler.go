4514107eef3b50a8cb5ac6cfef21c7750d1b7f53
Stanislav Petrov <s.e.petrov@gmail.com>
2020-08-18 09:14:14 +0300

    Use kubeless runtime go1.14

--- examples/post-go/handler.go
+++ examples/post-go/handler.go
@@ -6,7 +6,7 @@ import (
 	"github.com/kubeless/kubeless/pkg/functions"
 )
 
-// Handler sample function with data
+// Handler returns the given data.
 func Handler(event functions.Event, context functions.Context) (string, error) {
 	fmt.Println(event)
 	return event.Data, nil
