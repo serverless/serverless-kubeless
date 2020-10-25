920419f70ea7468e9d11430fcea6e81e66c1ae11
Stanislav Petrov <s.e.petrov@gmail.com>
2020-10-25 13:08:38 +0200

    Update kubectl to v1.19.2

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 install_kubectl() {
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.8/bin/linux/amd64/kubectl
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.19.2/bin/linux/amd64/kubectl
     chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
 }
